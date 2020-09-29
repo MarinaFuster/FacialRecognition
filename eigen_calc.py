e4cdca785073be316fd13be6df7377365fbbc98b
nachito <igvidaurreta@gmail.com>
2020-09-29 09:28:46 -0300

    Initial version of Householder QR, not working

---
+++ eigen_calc.py
@@ -0,0 +1,75 @@
+from math import sqrt
+import numpy as np
+from numpy.linalg import norm
+
+
+def mult_matrix(M, N):
+    """Multiply square matrices of same dimension M and N"""
+    # Converts N into a list of tuples of columns
+    tuple_N = zip(*N)
+
+    # Nested list comprehension to calculate matrix multiplication
+    return [[sum(el_m * el_n for el_m, el_n in zip(row_m, col_n)) for col_n in tuple_N] for row_m in M]
+
+
+def Q_i(Q_min, i, j, k):
+    """Construct the Q_t matrix by left-top padding the matrix Q
+    with elements from the identity matrix."""
+    if i < k or j < k:
+        return float(i == j)
+    else:
+        return Q_min[i-k][j-k]
+
+def householder(A):
+    """Performs a Householder Reflections based QR Decomposition of the
+    matrix A. The function returns Q, an orthogonal matrix and R, an
+    upper triangular matrix such that A = QR."""
+    n = len(A)
+
+    # Set R equal to A, and create Q as a zero matrix of the same size
+    R = A
+    Q = np.zeros(n, n)
+
+    # The Householder procedure
+    for k in range(n-1):  # We don't perform the procedure on a 1x1 matrix, so we reduce the index by 1
+        # Create identity matrix of same size as A
+        I = np.identity(n)
+
+        # Create the vectors x, e and the scalar alpha
+        # Python does not have a sgn function, so we use cmp instead
+        x = R[:, k] # kth column
+        e = I[:, k]
+        alpha = -np.sign(x[0]) * norm(x, 2)
+
+        u = x + alpha*e
+        v = u / norm(u)
+
+        # Create the Q minor matrix
+        Q_min = I - 2*v*np.transpose(v)
+
+        # "Pad out" the Q minor matrix with elements from the identity
+        Q_t = [[ Q_i(Q_min,i,j,k) for i in range(n)] for j in range(n)]
+
+        # If this is the first run through, right multiply by A,
+        # else right multiply by Q
+        if k == 0:
+            Q = Q_t
+            R = mult_matrix(Q_t,A)
+        else:
+            Q = mult_matrix(Q_t,Q)
+            R = mult_matrix(Q_t,R)
+
+    # Since Q is defined as the product of transposes of Q_t,
+    # we need to take the transpose upon returning it
+    return np.transpose(Q), R
+
+
+if __name__ == '__main__':
+    A = [[12, -51, 4], [6, 167, -68], [-4, 24, -41]]
+    Q, R = householder(A)
+
+    print(f"A: {A}")
+
+    print(f"Q: {Q}")
+
+    print(f"R: {R}")
