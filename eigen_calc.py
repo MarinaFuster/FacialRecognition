c52fea57cf9a321d4ea84984743353bd5bcd6206
nachito <igvidaurreta@gmail.com>
2020-09-29 20:20:35 -0300

    finish QR calculation with householder

--- eigen_calc.py
+++ eigen_calc.py
@@ -20,56 +20,31 @@ def Q_i(Q_min, i, j, k):
     else:
         return Q_min[i-k][j-k]
 
+
 def householder(A):
-    """Performs a Householder Reflections based QR Decomposition of the
-    matrix A. The function returns Q, an orthogonal matrix and R, an
-    upper triangular matrix such that A = QR."""
     n = len(A)
 
     # Set R equal to A, and create Q as a zero matrix of the same size
-    R = A
-    Q = np.zeros((n, n))
+    R = np.copy(A)
+    Q = np.identity(n)
 
     # The Householder procedure
-    for k in range(n-1):  # We don't perform the procedure on a 1x1 matrix, so we reduce the index by 1
-        # Create identity matrix of same size as A
-        I = np.identity(n)
+    for k in range(n):  # We don't perform the procedure on a 1x1 matrix, so we reduce the index by 1
+        v = R[k:, k]  # kth column
+        ul = v[0] + np.sign(v[0])*norm(v)
+        u = v/ul
+        u[0] = 1
+        beta = np.sign(v[0]) * ul / norm(v)
+        R[k:, :] = R[k:, :] - beta * np.outer(u, u).dot(R[k:])
+        Q[:, k:] = Q[:, k:] - beta * Q[:, k:].dot(np.outer(u, u))
 
-        # Create the vectors x, e and the scalar alpha
-        # Python does not have a sgn function, so we use cmp instead
-        x = R[:, k] # kth column
-        e = I[:, k]
-        alpha = -np.sign(x[0]) * norm(x, 2)
-
-        u = x + alpha*e
-        v = u / norm(u)
-
-        # Create the Q minor matrix
-        Q_min = I - 2*v*np.transpose(v)
-
-        # "Pad out" the Q minor matrix with elements from the identity
-        Q_t = [[ Q_i(Q_min,i,j,k) for i in range(n)] for j in range(n)]
-
-        # If this is the first run through, right multiply by A,
-        # else right multiply by Q
-        if k == 0:
-            Q = Q_t
-            R = mult_matrix(Q_t,A)
-        else:
-            Q = mult_matrix(Q_t,Q)
-            R = mult_matrix(Q_t,R)
-
-    # Since Q is defined as the product of transposes of Q_t,
-    # we need to take the transpose upon returning it
-    return np.transpose(Q), R
-
+    return Q.T, R
 
 if __name__ == '__main__':
-    A = [[12, -51, 4], [6, 167, -68], [-4, 24, -41]]
+    A = [[12.0, -51.0, 4.0], [6.0, 167.0, -68.0], [-4.0, 24.0, -41.0]]
+    A = np.array(A)
     Q, R = householder(A)
-
-    print(f"A: {A}")
-
     print(f"Q: {Q}")
-
     print(f"R: {R}")
+
+
