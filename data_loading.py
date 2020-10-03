c1d854d3430c578f77c9bbb22b44444601b0a072
jimenalozano <jilozano@itba.edu.ar>
2020-10-03 18:14:41 -0300

    testing preprocessing and classifier module

--- data_loading.py
+++ data_loading.py
@@ -1,20 +1,31 @@
 import numpy as np
 import os
 from PIL import Image
+import re
 
 # IMPORTANT
 # in this folder, only already functional faces will be stored.
 # If you need something like face detection, DO NOT store those images here
 DATA_PATH = "data/"
 
+
 def load_images():
     # Access all JPG files in directory
-    allfiles=os.listdir(DATA_PATH)
-    imlist=[filename for filename in allfiles if filename[-4:] in [".jpg",".JPG"]]
+    allfiles = os.listdir(DATA_PATH)
+    imlist = [filename for filename in allfiles if filename[-4:] in [".jpg", ".JPG"]]
+
+    # Translate names in JPG files to labels
+    names = [re.findall(r'[a-z]+', filename)[0] for filename in allfiles if filename[-4:] in [".jpg", ".JPG"]]
+    names = list(dict.fromkeys(names))
+    names.sort()
 
     images = []
+    labels = []
     for im in imlist:
         # images will be an array of (256,256,3) numpy arrays
-        images.append(np.array(Image.open(DATA_PATH + im),dtype=np.float))
+        images.append(np.array(Image.open(DATA_PATH + im), dtype=np.float))
+        # We assume that the first part of the image path that has only [a-z] characters is the face name
+        name = re.findall(r'[a-z]+', im)[0]
+        labels.append(names.index(name))
 
-    return np.array(images)
+    return np.array(images), np.array(labels), np.array(imlist), np.array(names)
--- main.py
+++ main.py
@@ -4,15 +4,19 @@ from PIL import Image
 from data_loading import load_images
 from preprocessing import PreProcessing, PCAPreprocessing
 from sklearn.decomposition import PCA
+from classifier import Classifier
+from sklearn.model_selection import train_test_split
 
-dataset = load_images()
+dataset, labels, paths, names = load_images()
 
-preprocessing = PreProcessing(dataset)
+dataset_train, dataset_test, labels_train, labels_test = train_test_split(dataset, labels, test_size=0.2)
+
+preprocessing = PreProcessing(dataset_train)
 
 C_matrix = np.dot(preprocessing.training_set, preprocessing.training_set.T)
 # print(C_matrix.shape)
 
-pca_module = PCA(n_components=dataset.shape[0])
+pca_module = PCA(n_components=dataset_train.shape[0])
 pca_module.fit(C_matrix)
 
 accumulated = 0
@@ -24,6 +28,20 @@ print(f"In order to win {round(accumulated,4)} variance ratio we will use {i} ei
 
 eigenvectors = pca_module.components_[list(range(0,i))]
 
+# Apply PCA transformation to training data
 pca_processing = PCAPreprocessing(preprocessing.training_set, preprocessing.avg_face, eigenvectors)
 
+# Train classifier with default C and gamma values
+classifier = Classifier()
+classifier.train_classifier(pca_processing.training_set, labels_train)
 
+# Apply PCA transformation to testing data
+# for data_i in dataset_test:
+
+# Test classifier
+y_pred = classifier.predict(dataset_test, labels_test)
+
+# To obtain a more readable output
+for i in range(len(y_pred)):
+    print("Predicting: ", paths[i], end ="")
+    print(". Face belongs to ... ", names[y_pred[i]])
