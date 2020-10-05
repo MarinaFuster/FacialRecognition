0678c7254af43fde86a8c278a436649e8d330c65
nachito <igvidaurreta@gmail.com>
2020-10-04 23:01:57 -0300

    minor code refactor and cli improvements

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 
+from eigen import calculate_eigenvectors
 from preprocessing import PreProcessing, PCAPreprocessing, KPCAPreprocessing
 from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
 
 from classifier import Classifier
 from data_loading import load_images
-import cv2
+from glob import glob
 
 
 def local_dataset():
@@ -19,15 +20,24 @@ def local_dataset():
 
     # Split dataset and labels in trainning and testing sets
     dataset_train, dataset_test, labels_train, labels_test = train_test_split(dataset, labels, test_size=0.2)
-    
+
     return dataset_train, dataset_test, labels_train, labels_test, names
+
+
+def preprocess_dataset(pca_processing, preprocessing, dataset):
+    ret_list = []
+    for data_i in dataset:
+        stnd_img = preprocessing.regular_preprocess(data_i)
+        ret_list.append(pca_processing.apply_pca(stnd_img))
+
+    return ret_list
 
 
 def train_with_svm(dataset_train, dataset_test, labels_train, labels_test, names=None):
     preprocessing = PreProcessing(dataset_train, dataset_train.shape[1], dataset_train.shape[2], dataset_train.shape[3])
 
     # Over this matrix we need to calculate eigenvectorss
-    #C_matrix = np.matmul(preprocessing.training_set, preprocessing.training_set.T)
+    # C_matrix = np.matmul(preprocessing.training_set, preprocessing.training_set.T)
     K = KPCAPreprocessing.rbf_kernel_pca(preprocessing.training_set)
     C_matrix = K
 
@@ -39,28 +49,22 @@ def train_with_svm(dataset_train, dataset_test, labels_train, labels_test, names
     i = 0
     while accumulated < 0.95:
         accumulated = accumulated + pca_module.explained_variance_ratio_[i]
-        i = i+1
-    print(f"In order to win {round(accumulated,4)} variance ratio we will use {i} eigenvectors")
+        i = i + 1
+    print(f"In order to win {round(accumulated, 4)} variance ratio we will use {i} eigenvectors")
 
-    eigenvectors = pca_module.components_[list(range(0,i))]
+    eigenvectors = pca_module.components_[list(range(0, i))]
+    # eigenvectors = calculate_eigenvectors(list(range(0, i)))
     # ... to here, must be replaced with eigenvectors calculated by eigen_calc
 
     # Apply PCA transformation to training data
-    pca_processing = PCAPreprocessing(preprocessing.training_set, preprocessing.avg_face, eigenvectors, \
-        dataset_train.shape[1], dataset_train.shape[2], dataset_train.shape[3])
-
-
+    pca_processing = PCAPreprocessing(preprocessing.training_set, preprocessing.avg_face, eigenvectors,
+                                      dataset_train.shape[1], dataset_train.shape[2], dataset_train.shape[3])
     # Train classifier with default C and gamma values
     classifier = Classifier()
     classifier.train_classifier(pca_processing.training_set, labels_train)
 
     # Apply PCA transformation to testing data
-    dataset_test_pca = []
-    for data_i in dataset_test:
-        stnd_img = preprocessing.regular_preprocess(data_i)
-        dataset_test_pca.append(pca_processing.apply_pca(stnd_img))
-
-
+    dataset_test_pca = preprocess_dataset(pca_processing, preprocessing, dataset_test)
     # Test classifier
     y_pred = classifier.predict(dataset_test_pca, labels_test)
 
@@ -70,20 +74,21 @@ def train_with_svm(dataset_train, dataset_test, labels_train, labels_test, names
 
     # To obtain a more readable output
     for i in range(len(y_pred)):
-        print("Predicting: ", names[labels_test[i]], end ="")
+        print("Predicting: ", names[labels_test[i]], end="")
         print(". Face belongs to ... ", names[int(y_pred[i])])
 
     corrects = 0
     for i in range(len(y_pred)):
         if labels_test[i] == y_pred[i]:
-            corrects = corrects+1
+            corrects = corrects + 1
     print(f"{corrects} out of {y_pred.shape[0]} were predicted properly")
     return classifier, preprocessing, pca_processing
 
 
 if __name__ == '__main__':
     dataset_train, dataset_test, labels_train, labels_test, names = local_dataset()
-    classifier, preprocessing, pca_processing = train_with_svm(dataset_train, dataset_test, labels_train, labels_test, names=names)
+    classifier, preprocessing, pca_processing = train_with_svm(dataset_train, dataset_test, labels_train, labels_test,
+                                                               names=names)
     should_end = False
     print("Enter path: (or write exit to exit)\n")
     while not should_end:
@@ -95,15 +100,19 @@ if __name__ == '__main__':
         if not file.exists():
             print("No such file <:(")
             continue
-
+        images = []
+        filenames = []
+        if file.is_dir():
+            filenames = glob(f"{path}/*.jpg")
+            for f in filenames:
+                images.append(Image.open(f))
+        else:
+            filenames = [path]
+            images.append(Image.open(path))
         # We assume its a path, not a directory (yet)
-        image = np.array(Image.open(path), dtype=np.float)
-        stnd_img = preprocessing.regular_preprocess(image)
-        processed_image = pca_processing.apply_pca(stnd_img)
-        y_pred = classifier.predict([processed_image])
-        print(names[y_pred])
-
-
-
-
-
+        count = 0
+        processed_images = preprocess_dataset(pca_processing, preprocessing, images)
+        for processed_image in processed_images:
+            y_pred = classifier.predict(processed_image)
+            print(f"{filenames[count]} predicted as {names[y_pred]}")
+            count += 1
